f1cb36fe1b6a1204bdc7e5a403f547d160f3b064
Boogeyman <colecolin619@gmail.com>
2023-05-08 02:26:42 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const icon = document.querySelector('.icon');
+            const search = document.querySelector('.search');
+            icon.onclick = function(){
+                search.classList.toggle('active')
+            }
+const element = document.getElementById("Login");
+element.onclick = doSomething;
+
+function doSomething(){
+    alert("You are Welcome to AEOMANIME!");
+}
